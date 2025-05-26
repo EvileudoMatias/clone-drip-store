8053da37d241a26ebbbd8741779467b1c41025bd
Evileudo Matias <evileudo@gmail.com>
2025-05-26 16:43:23 -0300

    atualizando header

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import "./Logo.css"
+
+export default function Logo(){
+  return(
+    <div className="box-logo">
+      <a href="">
+        <img src="../../../public/logo.svg" />
+      </a>
+      <h1>Drip Store</h1>
+    </div>
+  );
+}
