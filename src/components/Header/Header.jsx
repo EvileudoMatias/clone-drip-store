f5c709a4d521866be5c09c97d110a0a9fff80be4
Evileudo Matias <evileudo@gmail.com>
2025-05-28 16:17:59 -0300

    terminando o header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import { Logo } from '../Logo/Logo';
+import { Input } from '../Input/Input';
 import { Button } from '../Button/Button';
 import { ShopCart } from '../ShopCart/ShopCart';
 import './Header.css'
@@ -11,13 +12,21 @@ export const Header = () => {
       <div className='box-header'>
         <div className='box-header-children'>
           <Logo margin={'.5rem 0 0 .25rem'}/>
+          <Input 
+          width={'34.938rem'}
+          height={'3.75rem'}
+          margin={'0 3rem 0 1.688rem'}
+          type={'text'}
+          placeholder={'pesquisar produto...'}
+          src={'../../../public/search.svg'}
+          />
         </div>
         <Button 
          width={'7.125rem'} 
          height={'2.5rem'}
          margin={'2.75rem 0 0 1.875rem'}
          >Entrar</Button>
-         <ShopCart/>
+         <ShopCart value={7}/>
       </div>
     </header>
     </>
